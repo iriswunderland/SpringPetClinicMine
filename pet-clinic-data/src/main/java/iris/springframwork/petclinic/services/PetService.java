76c74e650ef68121f8823a6354888f5b71be0970
iris <iris.wunderland@gmail.com>
2019-09-22 23:35:09 +0200

    001_basic services for owner, pet and vet added

---
+++ pet-clinic-data/src/main/java/iris/springframwork/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package iris.springframwork.petclinic.services;
+
+import iris.springframwork.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}

de9299e99e86ae956b639152422d3f861c9e3988
iwieshofer <Iris.Wieshofer@wrwks.at>
2019-09-23 12:05:16 +0200

    001_adding base entity

--- pet-clinic-data/src/main/java/iris/springframwork/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/iris/springframwork/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package iris.springframwork.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
